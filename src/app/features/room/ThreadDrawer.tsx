c6e14b49d80e135ce2f3232f6611906e8424305f
Tomasz Sterna <tomasz@sterna.link>
2026-03-15 18:09:22 +0100

    Improve ThreadBrowser rendering

--- src/app/features/room/ThreadDrawer.tsx
+++ src/app/features/room/ThreadDrawer.tsx
@@ -724,6 +724,7 @@ export function ThreadDrawer({ room, threadRootId, onClose, overlay }: ThreadDra
           <RoomViewFollowingPlaceholder />
         ) : (
           <RoomViewFollowing
+            style={{ backgroundColor: 'transparent' }}
             room={room}
             threadEventId={latestThreadEventId}
             participantIds={threadParticipantIds}
